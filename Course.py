6e6d66b6795ac2a83971e3fc88b49c688da91cb5
Melvin <Melvin@DESKTOP-RJLA8CT.home>
2017-04-12 22:05:52 -0400

    Skeleton for Course and schedulePicker

---
+++ Course.py
@@ -0,0 +1,11 @@
+class Course:
+	
+	title = "Default course name"
+	nbr = 0
+	prof = "Default professor name"
+	time = [][][]
+
+	def __init__(self):
+		
+	def print():
+		
---
+++ schedulePicker.py
@@ -0,0 +1,20 @@
+from .Course import Course
+
+courses = []
+schedules = [][]
+
+def initCourses(datain):
+	
+def determineSchedules(courses):
+	
+def determineOverlap(course1, course2):
+	
+def printSchedules(schedules):
+	
+def main(datain):
+	data = '''get data from file'''
+	initCourses(data)
+	determineSchedules(courses)
+	printSchedules(schedules)
+
+main('''input from textfile''')

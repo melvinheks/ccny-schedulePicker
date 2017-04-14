462d972748bafa18d8a50f00d5c4a63d99c599d4
Melvin <Melvin@DESKTOP-RJLA8CT.home>
2017-04-14 13:17:27 -0400

    Added: dayTimes(), courseNbr() Removed: print() print() is reserved in python

--- Course.py
+++ Course.py
@@ -9,9 +9,17 @@ class Course:
 		time: A dictionary of an array of tuples that is used to retrieve all of the course times. The first key is the day, the first index is the occurrence i.e. there are some courses that have different times on the same day, the second index is the start and end tuple. E.g. time["monday"][0][START] will return the start time for a Course's monday class.
 	"""
 	def __init__(self, title, nbr, prof, time):
+		"""Inititalizes Course object"""
 		self.title = title
 		self.nbr = nbr
 		self.prof = prof
 		self.time = time
-	def print():
-		
+	def dayTimes(self, day):
+		"""Returns a string array of all day times for the Course"""
+		s = []
+		for occurrences in time[day] :
+			s.append("{}: {}-{}\n".format(day, occurrences[0], occurrences[1]))
+		return s
+	def courseNbr(self):
+		"""Returns a string with nbr"""
+		return "Course Number:{}".format(self.nbr)
